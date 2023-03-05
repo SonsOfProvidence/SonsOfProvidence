8313448d1d43181443f30497f816efc4f8b200fe
Danny Bierek <dannybierek@gmail.com>
2023-03-05 01:08:22 -0800

    Add airplane model and level. Testing it in sapienza. Added options to manifest. Testing Agent 47 as NPC in Dartmoor

--- runScriptsSonsOfProvidence.bat
+++ runScriptsSonsOfProvidence.bat
@@ -4,4 +4,7 @@ cd "C:\Program Files (x86)\Steam\steamapps\common\HITMAN 3\Simple Mod Framework\
 Deploy.exe
 cd "C:\Program Files (x86)\Steam\steamapps\common\HITMAN 3\"
 Launcher.exe
+cd "C:\Program Files (x86)\Steam\steamapps\common\HITMAN 3\Retail"
+del thumbs.dat
+copy "C:\Program Files (x86)\Steam\steamapps\common\HITMAN 3\Simple Mod Framework\Mods\Sons of Providence\thumbs_funeral.dat" thumbs.dat
 cd "C:\Program Files (x86)\Steam\steamapps\common\HITMAN 3\Simple Mod Framework\Mods\Sons of Providence\"
