8db81f6ba664238fffb6f709b00fe10ea86c6ed5
Danny Bierek <dannybierek@gmail.com>
2023-03-12 00:40:56 -0800

    Added outfits for airplane. Added Eugene Cobb outfit and head and tie and shirt. Updated plane mesh with door and handle and widened. Added repo entry. Removed Marcus. Added PFObstacle.

--- runScriptsSonsOfProvidence.bat
+++ runScriptsSonsOfProvidence.bat
@@ -1,7 +1,8 @@
 cd "C:\Program Files (x86)\Steam\steamapps\common\HITMAN 3\Simple Mod Framework\Mods\Sons of Providence\"
 call npx prettier --write **/*.json  --end-of-line crlf
 cd "C:\Program Files (x86)\Steam\steamapps\common\HITMAN 3\Simple Mod Framework\"
-Deploy.exe
+rem Deploy.exe
+Deploy.exe --logLevel verbose --logLevel debug --logLevel info --logLevel warn --logLevel error
 cd "C:\Program Files (x86)\Steam\steamapps\common\HITMAN 3\"
 Launcher.exe
 cd "C:\Program Files (x86)\Steam\steamapps\common\HITMAN 3\Retail"
